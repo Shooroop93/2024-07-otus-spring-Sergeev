dd590ce61f64b878c819def0eb8bb7e268cc7ecf
Roman Sergeev <roman.sergeev1993@gmail.com>
2024-08-06 00:27:20 +0300

    наработки

--- hw01/src/main/java/ru/otus/hw/Application.java
+++ hw01/src/main/java/ru/otus/hw/Application.java
@@ -2,15 +2,23 @@ package ru.otus.hw;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.otus.hw.service.TestRunnerService;
+import ru.otus.hw.config.AppProperties;
+import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
 
         //Прописать бины в spring-context.xml и создать контекст на его основе
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-        var testRunnerService = context.getBean(TestRunnerService.class);
-        testRunnerService.run();
+//        var testRunnerService = context.getBean(TestRunnerService.class);
+//        testRunnerService.run();
+
+        CsvQuestionDao csvQuestionDao = new CsvQuestionDao((AppProperties) context.getBean("appProperties"));
+        List<Question> all = csvQuestionDao.findAll();
+        System.out.println();
 
     }
 }
--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,5 +1,9 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.ColumnPositionMappingStrategyBuilder;
+import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
@@ -7,10 +11,12 @@ import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.io.*;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
@@ -18,26 +24,29 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
+        List<QuestionDto> questionDtoList = getQuestionDtoList(fileNameProvider.getTestFileName());
+        return getAListOfQuestion(questionDtoList);
+    }
 
+    private List<Question> getAListOfQuestion(List<QuestionDto> questionDtoList) {
         List<Question> result = new ArrayList<>();
+        questionDtoList.forEach(i -> result.add(i.toDomainObject()));
+        return result;
+    }
 
-        try (InputStream resourceAsStream = getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName());
-             BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(resourceAsStream)))) {
+    private List<QuestionDto> getQuestionDtoList(String fileNameProvider) {
+        try (InputStream file = getClass().getClassLoader().getResourceAsStream(fileNameProvider);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(file))) {
 
-            List<QuestionDto> listQuestionDto = new CsvToBeanBuilder(reader)
+            List<QuestionDto> parse = new CsvToBeanBuilder(reader)
                     .withSkipLines(1)
                     .withType(QuestionDto.class)
                     .build()
                     .parse();
-
-            for (QuestionDto questionDto : listQuestionDto) {
-                result.add(questionDto.toDomainObject());
-            }
+            return parse;
 
         } catch (IOException e) {
             throw new QuestionReadException(e.getMessage());
         }
-
-        return result;
     }
 }
